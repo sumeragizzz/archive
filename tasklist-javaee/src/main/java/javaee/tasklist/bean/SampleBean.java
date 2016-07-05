7d211d6d1fc44413e9fcfea34da104868bb0df52
sumeragizzz <forester@cocoa.plala.or.jp>
2016-07-06 00:56:17 +0900

    サンプルJSF

---
+++ tasklist-javaee/src/main/java/javaee/tasklist/bean/SampleBean.java
@@ -0,0 +1,33 @@
+package javaee.tasklist.bean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@RequestScoped
+@Named
+public class SampleBean {
+
+    private String text;
+
+    public SampleBean() {
+    }
+
+    public String action1() {
+        setText("action1");
+        return null;
+    }
+
+    public String action2() {
+        setText("action2");
+        return null;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
