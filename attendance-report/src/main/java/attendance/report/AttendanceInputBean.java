8626640839124d2739857187ad5f8d57592ddb5c
sumeragizzz <forester@cocoa.plala.or.jp>
2017-07-10 19:59:06 +0900

    入力画面実装開始

---
+++ attendance-report/src/main/java/attendance/report/AttendanceInputBean.java
@@ -0,0 +1,22 @@
+package attendance.report;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class AttendanceInputBean {
+
+    public AttendanceInputBean() {
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+    }
+}
